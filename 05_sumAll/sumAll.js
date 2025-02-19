a4aecedb30f0f3a96f9af9a3bb862baf84c6e7cb
seejl25 <seejunlong@u.nus.edu>
2025-02-19 23:14:51 +0800

    Javascript practices using arrays, loops, if-else, operators

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    let finalSum = 0;
+    if (typeof arg1 !== "number" || arg1%1!==0 || typeof arg2 != "number" || arg2%1 !==0) {
+        return "ERROR";
+    }
+    if (arg1 <0 || arg2 <0) {
+        return "ERROR";
+    }
+    for (let i = Math.min(arg1,arg2); i <= Math.max(arg1,arg2); i++) {
+        finalSum += i;
+    }
+    return finalSum
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  let newTempInC = (tempInF-32) * (5/9);
+  newTempInC = Math.round(newTempInC*10)/10;
+  return newTempInC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  let newTempInF = tempInC*(9/5)+32;
+  newTempInF = Math.round(newTempInF*10)/10;
+  return newTempInF;
 };
 
 // Do not edit below this line
